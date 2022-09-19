fae7acfbf19b77bab17d796a01751bac9c8a4a5b
isacchr <isac.christiansson@gmail.com>
2022-09-19 13:05:56 +0200

    refactored to information writes in a p tag instead of console.log

--- public/app.js
+++ public/app.js
@@ -5,9 +5,12 @@ function primtalLista() {
     var txtBoxValue = document.getElementById('nrInputLista').value;
     const primtalArray = new Array(2,3,5,7,11);
 
+    var textParagraph = document.getElementById('primtalListaText');
+    textParagraph.innerHTML = "De första " + txtBoxValue + " primtalen är: ";
+
     for (var i = 0; i < txtBoxValue; i++) {
 
-        console.log(primtalArray[i]);
+        textParagraph.innerHTML += primtalArray[i] + ", ";
 
     };
 
@@ -18,8 +21,13 @@ function primtalPosition() {
     var txtBoxValue = document.getElementById('nrInputPrimtal').value;
     const primtalArray = new Array(2,3,5,7,11);
 
+    const textParagraph = document.getElementById('primtalPositionText');
+
     txtBoxValue -= 1;
 
-    console.log(primtalArray[txtBoxValue]);
+    textParagraph.innerHTML = "Det " + txtBoxValue + " primtalet är: ";
+
+    textParagraph.innerHTML += primtalArray[txtBoxValue];
+
 }
 
