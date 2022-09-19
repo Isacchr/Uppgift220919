7d8e27c072d66f2353da405ca4a946a46f5f3c85
isacchr <isac.christiansson@gmail.com>
2022-09-19 12:32:40 +0200

    new array added and for loop added to write out numbers from array

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-console.log('hej from js');
+console.log('hej from js')
+
+function primtalLista() {
+ 
+    var txtBoxValue = document.getElementById('nrInputLista').value;
+    const primtalArray = new Array(2,3,5,7,11);
+
+    for (var i = 0; i < txtBoxValue; i++) {
+
+        console.log(primtalArray[i]);
+
+    };
+
+}
+
